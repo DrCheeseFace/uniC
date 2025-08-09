f8685395e61dc81346e1521147bd1cc0cca28d39
drcheeseface <tharun1@hotmail.co.uk>
2025-08-09 15:27:10 +0530

    chore: test util tweaks

--- test/test.c
+++ test/test.c
@@ -1,55 +1,59 @@
-#include "t_utils.h"
+#include "test_utils.h"
 
 void foo(void)
 {
-	struct T_Context *t_ctx = T_ctx_create("my first context");
+	struct TEST_Context *t_ctx = TEST_ctx_create("my first context");
 
 	int expected = 3;
 	int actual = 2;
-	struct T_Case test_case = { .description = "test case 1",
-				    .pass = T_ASSERT_EQ(expected, actual) };
+	struct TEST_Case test_case = { .description = "test case 1",
+				       .pass = TEST_ASSERT_EQ(expected,
+							      actual) };
 
-	T_ctx_append_test(t_ctx, test_case);
+	TEST_ctx_append_case(t_ctx, test_case);
 
 	expected = 3;
 	actual = 3;
-	test_case = (struct T_Case){ .description = "test case 2",
-				     .pass = T_ASSERT_EQ(expected, actual) };
-	T_ctx_append_test(t_ctx, test_case);
+	test_case =
+		(struct TEST_Case){ .description = "test case 2",
+				    .pass = TEST_ASSERT_EQ(expected, actual) };
+	TEST_ctx_append_case(t_ctx, test_case);
 
-	expected = true;
-	test_case = (struct T_Case){ .description = "test case 3",
-				     .pass = T_ASSERT(expected) };
-	T_ctx_append_test(t_ctx, test_case);
+	actual = true;
+	test_case = (struct TEST_Case){ .description = "test case 3",
+					.pass = TEST_ASSERT(actual) };
+	TEST_ctx_append_case(t_ctx, test_case);
 
-	T_ctx_log(*t_ctx);
-	T_ctx_destroy(t_ctx);
+	TEST_ctx_log(*t_ctx);
+	TEST_ctx_destroy(t_ctx);
 }
 
 void bar(void)
 {
-	struct T_Context *t_ctx = T_ctx_create("my second context");
+	struct TEST_Context *t_ctx = TEST_ctx_create("my second context");
 
 	int expected = 3;
 	int actual = 3;
-	struct T_Case test_case = { .description = "test case 1",
-				    .pass = T_ASSERT_EQ(expected, actual) };
+	struct TEST_Case test_case = { .description = "test case 1",
+				       .pass = TEST_ASSERT_EQ(expected,
+							      actual) };
 
-	T_ctx_append_test(t_ctx, test_case);
+	TEST_ctx_append_case(t_ctx, test_case);
 
 	expected = 3;
 	actual = 3;
-	test_case = (struct T_Case){ .description = "test case 2",
-				     .pass = T_ASSERT_EQ(expected, actual) };
-	T_ctx_append_test(t_ctx, test_case);
+	test_case =
+		(struct TEST_Case){ .description = "test case 2",
+				    .pass = TEST_ASSERT_EQ(expected, actual) };
+	TEST_ctx_append_case(t_ctx, test_case);
 
-	expected = true;
-	test_case = (struct T_Case){ .description = "test case 3",
-				     .pass = T_ASSERT(expected) };
-	T_ctx_append_test(t_ctx, test_case);
+	actual = true;
+	test_case = (struct TEST_Case){ .description = "test case 3",
+					.pass = TEST_ASSERT(actual) };
+	TEST_ctx_append_case(t_ctx, test_case);
 
-	T_ctx_log(*t_ctx);
-	T_ctx_destroy(t_ctx);
+	TEST_ctx_log(*t_ctx);
+	TEST_ctx_destroy(t_ctx);
 }
 
 int main(void)
---
+++ test/test_utils.h
@@ -0,0 +1,58 @@
+#ifndef TEST_UTILS_H
+#define TEST_UTILS_H
+
+#include <stdbool.h>
+#include <sys/types.h>
+
+#define TEST_DEFAULT_COLOR_CODE "\x1b[0m"
+#define TEST_BLUE_COLOR_CODE "\x1b[34m"
+#define TEST_GREEN_COLOR_CODE "\x1b[32m"
+#define TEST_RED_COLOR_CODE "\x1b[31m"
+
+#define TEST_MAX_TEST_CASES_PER_CONTEXT 10
+#define TEST_MAX_CONTEXT_DESCRIPTION 100
+
+#define TEST_TAB "    "
+
+// use me!
+#define TEST_ASSERT_EQ(expected, actual)                                       \
+	(TEST_assert_eq((&expected), (&actual), (sizeof(expected))))
+
+#define TEST_ASSERT(actual)                                                    \
+	(TEST_assert_eq(&(bool){ actual }, &(bool){ true }, sizeof(bool)))
+
+struct TEST_Case {
+	char description[TEST_MAX_CONTEXT_DESCRIPTION];
+	bool pass;
+};
+
+struct TEST_Context {
+	char description[TEST_MAX_CONTEXT_DESCRIPTION];
+	int pass_count;
+	int fail_count;
+	char *failed_test_descriptions[TEST_MAX_TEST_CASES_PER_CONTEXT];
+
+	struct TEST_Case cases[TEST_MAX_TEST_CASES_PER_CONTEXT];
+	int case_count;
+};
+
+typedef enum {
+	TEST_SEVERITY_DEFAULT,
+	TEST_SEVERITY_INFO,
+	TEST_SEVERITY_OK,
+	TEST_SEVERITY_ERROR,
+	TEST_SEVERITY_COUNT
+} TEST_Severity;
+
+bool TEST_assert_eq(void *expected, void *actual, size_t size_of);
+
+struct TEST_Context *TEST_ctx_create(const char *description);
+
+void TEST_ctx_destroy(struct TEST_Context *t_ctx);
+
+void TEST_ctx_append_case(struct TEST_Context *t_ctx,
+			  struct TEST_Case test_case);
+
+void TEST_ctx_log(struct TEST_Context t_ctx);
+
+#endif // !TEST_UTILS_H
