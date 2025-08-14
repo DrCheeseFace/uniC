9d0e077ecb24ce8e5ce39e5a6981e7dcfd58cbca
tharun11 <tharun@ctrlprint.se>
2025-08-14 08:51:27 +0530

    fix: add missing valid variable name chars chars

--- src/f_file.c
+++ src/f_file.c
@@ -17,7 +17,9 @@ const char *tokens_to_str[F_CTOKENS_COUNT] = {
 const char valid_variable_name_chars[VALID_VARIABLE_NAME_CHARS_COUNT] = {
 	'1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'a', 'b', 'c',
 	'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
-	'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '_',
+	'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C',
+	'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+	'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '_',
 };
 
 int F_get_file_prefix(MRS_String *file_name, MRS_String *dest)
